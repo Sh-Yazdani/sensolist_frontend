dacdf85e31e6643088c6f7a3969b198f50a7f4d9
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-06-20 10:02:40 +0300

    feat: initialize react hook form

--- src/app/authentication/login/page.tsx
+++ src/app/authentication/login/page.tsx
@@ -1,9 +1,11 @@
 import FormHeader from "@/components/authentication/FormHeader";
+import LoginForm from "@/components/authentication/LoginForm";
 
 export default function Page() {
   return (
     <>
       <FormHeader title="Login" description="Please enter your details." />
+      <LoginForm />
     </>
   );
 }
