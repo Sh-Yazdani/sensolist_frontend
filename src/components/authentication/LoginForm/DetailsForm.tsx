45a5bef83d3ff7e585d2e625adfc6ddd782a25c4
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-06-20 12:04:15 +0300

    feat: phone number input

--- src/components/authentication/LoginForm/DetailsForm.tsx
+++ src/components/authentication/LoginForm/DetailsForm.tsx
@@ -1,12 +1,13 @@
 import { LoginInputs } from "@/app/types/general";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { TextInput } from "../TextInput";
+import { CustomPhoneInput } from "../PhoneInput/PhoneInput";
 
 export default function DetailsForm() {
   const {
     register,
     handleSubmit,
     watch,
+    control,
     formState: { errors },
   } = useForm<LoginInputs>();
 
@@ -19,7 +20,11 @@ export default function DetailsForm() {
       onSubmit={handleSubmit(onSubmit)}
       className="w-full mt-10 md:mt-14 lg:mt-20"
     >
-      <TextInput register={register} name="phoneNumber" label="Phone number" />
+      <CustomPhoneInput
+        register={register}
+        name="phoneNumber"
+        label="Phone number"
+      />
       {/* <TextInput defaultValue="" {...register("password", { required: true })} /> */}
     </form>
   );
--- src/components/authentication/PhoneInput/PhoneInput.tsx
+++ src/components/authentication/PhoneInput/PhoneInput.tsx
@@ -1,28 +1,27 @@
 import { LoginInputs } from "@/app/types/general";
 import { UseFormRegister } from "react-hook-form";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 
 interface InputProps {
   register: UseFormRegister<LoginInputs>;
   name: keyof LoginInputs;
-  defaultValue?: string;
   label: string;
 }
 
-export function TextInput({
+export function CustomPhoneInput({
+  label,
   register,
   name,
-  defaultValue,
-  label,
   ...rest
 }: InputProps) {
   return (
     <label className="w-full">
       <span className="mb-2 md:text-xl">{label}</span>
-      <input
-        className="w-full bg-transparent border border-white-opacity-800 
-        bg-gradient-opacity rounded-lg h-[45px] md:h-[60px] focus-visible:outline-none"
-        {...register(name)}
-        {...rest}
+      <PhoneInput
+        placeholder=""
+        inputProps={{ ...register(name) }}
+        inputClass="!h-[45px] lg:!h-[60px] !w-full"
       />
     </label>
   );
