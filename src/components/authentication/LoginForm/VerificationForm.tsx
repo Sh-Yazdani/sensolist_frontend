ffd749606a430bace6f9bcef56b998a489c3b0d2
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-06-22 15:17:10 +0300

    feat: verification input error display

--- src/components/authentication/LoginForm/VerificationForm.tsx
+++ src/components/authentication/LoginForm/VerificationForm.tsx
@@ -1,16 +1,35 @@
+import { Warning2 } from "iconsax-react";
 import VerificationInput from "react-verification-input";
 import SubmitButton from "../SubmitButton";
 import TimerCountDown from "../TimerCountdown/index.tsx";
-export default function VerificationForm() {
+
+interface VerificationFormProps {
+  error?: string;
+}
+
+export default function VerificationForm({ error }: VerificationFormProps) {
   return (
-    <div className="flex flex-col items-center justify-center mt-20">
-      <VerificationInput
-        length={5}
-        placeholder=""
-        classNames={{
-          container: "lg:w-[528px] h-[80px] justify-between",
-        }}
-      />
+    <div className=" flex flex-col items-center justify-center mt-20">
+      <div className="relative">
+        <VerificationInput
+          length={5}
+          placeholder=""
+          classNames={{
+            container: "lg:w-[528px] h-[80px] justify-between",
+          }}
+          inputProps={{
+            className: "bg-error",
+          }}
+        />
+        {error && (
+          <div
+            className="rounded bg-error text-sm text-white py-1 px-2 flex items-center
+        absolute bottom-[-2.25rem]"
+          >
+            <Warning2 /> {error}
+          </div>
+        )}
+      </div>
       <TimerCountDown />
       <SubmitButton className="mt-20 lg:mb-[142px]">Login</SubmitButton>
     </div>
