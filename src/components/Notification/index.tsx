41b2c453042728245cc4c3932ddd9c056390ccab
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-06-23 12:31:54 +0300

    feat: display the alert message

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,12 +1,35 @@
 "use client";
 
 import { RootState } from "@/lib/store";
+import { INotificationAlert } from "@/types/general";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function Notification() {
   const { alerts } = useSelector(
     (state: RootState) => state.notificationReducer
   );
+
+  const [alert, setAlert] = useState<INotificationAlert>({
+    type: "success",
+    message: "",
+  });
+  const [show, setShow] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (alerts.length > 0) {
+      setAlert(alerts[alerts.length - 1]);
+      setShow(true);
+      setTimeout(() => {
+        setShow(false);
+      }, 3000);
+    }
+  }, [alerts]);
+
+  const onClose = () => {
+    setShow(false);
+  };
+
   console.log("state", alerts);
-  return <></>;
+  return show ? <div>{alert.message}</div> : null;
 }
