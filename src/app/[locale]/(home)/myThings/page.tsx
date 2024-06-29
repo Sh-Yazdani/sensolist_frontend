26e3a6430edcdcb4c33d27be41615ac764eead12
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-06-29 11:11:37 +0300

    feat: sortby dropdown

--- src/app/[locale]/(home)/myThings/page.tsx
+++ src/app/[locale]/(home)/myThings/page.tsx
@@ -2,7 +2,7 @@ import SortBy from "@/components/SortBy";
 
 export default function Page() {
   return (
-    <div className=" flex flex-col">
+    <div className=" flex flex-col pt-4 lg:pt-0 px-4">
       <div className="flex items-center">
         <SortBy />
         {/* <div>search bars</div> */}
--- src/components/SortBy/index.tsx
+++ src/components/SortBy/index.tsx
@@ -6,12 +6,16 @@ import { useState } from "react";
 export default function SortBy() {
   const values: string[] = ["newest", "oldest", "name"];
   const [value, setValue] = useState<string>(values[0]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <div className="flex items-center">
+    <div className="flex items-center relative">
       <span className="hidden lg:flex text-xl mr-2 text-neutral-7 dark:text-neutral-3">
         Sort By:
       </span>
       <button
+        onClick={() => {
+          setIsOpen((prev) => !prev);
+        }}
         className=" lg:text-xl text-neutral-7 rounded-full p-2 lg:px-4 lg:py-3
       border-2 border-neutral-3 dark:border-none
       dark:bg-white-opacity-100 dark:text-neutral-3 flex gap-2"
@@ -19,6 +23,21 @@ export default function SortBy() {
         <span className=" capitalize">{value}</span>
         <ArrowDown2 />
       </button>
+      {isOpen && (
+        <div className=" absolute shadow rounded-lg bg-neutral-2 dark:bg-primary w-[153px] lg:w-[200px] overflow-hidden left-0 top-12 lg:top-16">
+          {values.map((val: string, i: number) => (
+            <div
+              key={val}
+              className={`py-2 lg:py-4 capitalize text-center text-neutral-7 dark:text-neutral-3 ${
+                i !== values.length - 1 &&
+                "border-b border-neutral-4 dark:border-neutral-7"
+              }`}
+            >
+              {val}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
