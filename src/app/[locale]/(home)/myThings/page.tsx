dbd72be3c66216cee611469f9bf76508d4c1fd08
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-06-29 12:37:43 +0300

    feat: searchbar large screens and dark mode

--- src/app/[locale]/(home)/myThings/page.tsx
+++ src/app/[locale]/(home)/myThings/page.tsx
@@ -4,7 +4,7 @@ import SortBy from "@/components/SortBy";
 export default function Page() {
   return (
     <div className=" flex flex-col pt-20 lg:pt-0 px-4">
-      <div className="md:relative">
+      <div className="md:relative flex flex-row-reverse justify-end">
         <SearchBar />
         <SortBy />
         {/* <div>search bars</div> */}
--- src/components/SortBy/index.tsx
+++ src/components/SortBy/index.tsx
@@ -38,7 +38,7 @@ export default function SortBy() {
 
   return (
     <div className="flex items-center relative">
-      <span className="hidden lg:flex text-xl mr-2 text-neutral-7 dark:text-neutral-3">
+      <span className="hidden lg:flex text-xl mr-2 text-neutral-7 dark:text-neutral-3 whitespace-nowrap">
         Sort By:
       </span>
       <button
@@ -47,10 +47,10 @@ export default function SortBy() {
         }}
         className=" lg:text-xl text-neutral-7 rounded-full p-2 lg:px-4 lg:py-3
       border-2 border-neutral-3 dark:border-none
-      dark:bg-white-opacity-100 dark:text-neutral-3 flex gap-2"
+      dark:bg-white-opacity-100 dark:text-neutral-3 flex gap-2 min-w-[140px]"
       >
         <span className=" capitalize">{value}</span>
-        <ArrowDown2 />
+        <ArrowDown2 className="ml-auto" />
       </button>
       {isOpen && (
         <div className=" absolute shadow rounded-lg bg-neutral-2 dark:bg-primary w-[153px] lg:w-[200px] overflow-hidden left-0 top-12 lg:top-16 flex flex-col">
