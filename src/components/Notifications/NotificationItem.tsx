9b179b708436172c52853860edde2d65084bb410
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-01 20:46:57 +0300

    feat: resize notification

--- src/components/Notifications/NotificationItem.tsx
+++ src/components/Notifications/NotificationItem.tsx
@@ -27,15 +27,15 @@ export default function NotificationItem(props: NotificationItemProps) {
   return (
     <div className="w-full border-b border-neutral-6 dark:border-neutral-8 py-4 flex flex-col">
       <div className="flex flex-row items-center">
-        <div className="w-10 h-10">
+        <div className="w-8 h-8">
           {props.new ? (
-            <MessageNotif className=" size-8 lg:size-10 text-secondary-main" />
+            <MessageNotif className=" size-6 lg:size-8 text-secondary-main" />
           ) : (
-            <Message className=" size-8 lg:size-10 text-neutral-7" />
+            <Message className=" size-6 lg:size-8 text-neutral-7" />
           )}
         </div>
         <div
-          className={`line-clamp-2 text-left ml-2 ${
+          className={`line-clamp-2 text-left ml-2 text-sm ${
             props.new
               ? " text-neutral-8 dark:text-neutral-3"
               : "text-neutral-6 dark:text-neutral-7"
@@ -49,7 +49,7 @@ export default function NotificationItem(props: NotificationItemProps) {
           props.new
             ? " text-neutral-8 dark:text-neutral-3"
             : "text-neutral-6 dark:text-neutral-7"
-        } text-neutral-6 dark:text-neutral-7 w-fit ml-auto mt-2 text-xs`}
+        } text-neutral-6 dark:text-neutral-7 w-fit ml-auto mt-2 text-[10px]`}
       >
         {timePast}
       </div>
