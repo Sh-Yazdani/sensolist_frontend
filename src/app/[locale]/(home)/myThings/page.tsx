08625dfa9d1cd30579fd86531a15803c763c4f8e
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-02 12:52:26 +0300

    fix: resize my thing cards

--- src/app/[locale]/(home)/myThings/page.tsx
+++ src/app/[locale]/(home)/myThings/page.tsx
@@ -123,7 +123,7 @@ export default function Page() {
         <FilterComponent />
         <SortBy />
       </div>
-      <div className="mt-4 flex flex-wrap justify-between lg:mr-[292px]">
+      <div className="mt-4 flex flex-wrap lg:mr-[292px]">
         {fakeThings.map((thing) => (
           <MyThingCard key={thing.id} thing={thing} />
         ))}
