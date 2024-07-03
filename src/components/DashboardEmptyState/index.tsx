244eda233c20e17f2de831378e6ef869295d5e5a
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-03 10:11:32 +0300

    feat: empty state dark mode

--- src/components/DashboardEmptyState/index.tsx
+++ src/components/DashboardEmptyState/index.tsx
@@ -8,7 +8,7 @@ export default function DashboardEmptyState() {
       <SimplificationIcon className="w-[56px] h-[47px] md:w-[80px] md:h-[67px] lg:w-[112px] lg:h-[94px]" />
       <div
         className="w-[180px] text-neutral-5 text-xs text-center mt-4 
-      md:text-base md:w-[330px] md:mt-6 lg:text-lg lg:w-[370px]"
+      md:text-base md:w-[330px] md:mt-6 lg:text-lg lg:w-[370px] dark:text-neutral-6"
       >
         You haven’t created any dashboard yet!
       </div>
