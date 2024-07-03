502c034b32ef1d31535d4a43fea781260cceb3b0
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-03 09:18:14 +0300

    feat: dashboard sort and search component

--- src/app/[locale]/(home)/dashboard/page.tsx
+++ src/app/[locale]/(home)/dashboard/page.tsx
@@ -1,3 +1,13 @@
+import SearchBar from "@/components/SearchBar";
+import SortBy from "@/components/SortBy";
+
 export default function Page() {
-  return <div>dashboard</div>;
+  return (
+    <div className=" flex flex-col pt-20 md:pt-0 md:pr-4 px-4">
+      <div className="md:relative flex flex-row-reverse justify-end">
+        <SearchBar />
+        <SortBy />
+      </div>
+    </div>
+  );
 }
