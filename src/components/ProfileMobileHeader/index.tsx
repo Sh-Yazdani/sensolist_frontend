fc952eee5e04e5e00cd356158a29aa11aba4a189
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-06 12:24:38 +0300

    fix: mobile header text

--- src/components/ProfileMobileHeader/index.tsx
+++ src/components/ProfileMobileHeader/index.tsx
@@ -1,16 +1,22 @@
 "use client";
 
 import { ArrowLeft } from "iconsax-react";
+import { usePathname } from "next/navigation";
 
 export default function ProfileMobileHeader({
   onGoBack,
 }: {
   onGoBack: () => void;
 }) {
+  const pathname = usePathname();
   return (
     <div className="w-full rounded-b-2xl bg-primary-tint-1 h-[80px] absolute top-[-80px] flex md:hidden z-20">
       <div className=" capitalize mx-auto text-white font-medium mt-6">
-        Product’s details
+        {pathname === "/profile"
+          ? "edit profile"
+          : pathname === "/profile/changePassword"
+          ? "change password"
+          : "notifications"}
       </div>
       <button
         onClick={onGoBack}
