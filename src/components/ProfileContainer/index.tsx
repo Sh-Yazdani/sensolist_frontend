2c190931530177c9ee49c78cfbe695f295d07379
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-07 12:11:10 +0300

    feat: form container max width

--- src/components/ProfileContainer/index.tsx
+++ src/components/ProfileContainer/index.tsx
@@ -23,7 +23,7 @@ export default function ProfileContainer({ children }: ProfileContainerProps) {
           dispatch(toggleMenu({ menuOpen: false }));
         }}
       />
-      <div className="flex-1 md:mr-[52px] lg:mr-10 rounded-r-2xl shadow-md-right-300 shadow-md-right-white-150 ">
+      <div className="flex-1 md:mr-[52px] lg:mr-10 rounded-r-2xl shadow-md-right-300 shadow-md-right-white-150 max-w-[576px]">
         {!menuOpen && (
           <ProfileMobileHeader
             onGoBack={() => {
