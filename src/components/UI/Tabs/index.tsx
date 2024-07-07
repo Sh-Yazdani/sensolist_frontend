05b1b0faf2353b4bc84ce121fb2f752ce1b0b13e
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-07 14:16:14 +0300

    feat: dark mode

--- src/components/UI/Tabs/index.tsx
+++ src/components/UI/Tabs/index.tsx
@@ -6,7 +6,7 @@ interface TabsProps {
 
 export default function Tabs({ items, currentIndex, onTabChange }: TabsProps) {
   return (
-    <div className="relative flex items-center border-b-2 border-neutral-3 w-full">
+    <div className="relative flex items-center border-b-2 border-neutral-3 dark:border-neutral-8 w-full">
       {items.map((item, i) => (
         <button
           onClick={() => {
@@ -18,15 +18,15 @@ export default function Tabs({ items, currentIndex, onTabChange }: TabsProps) {
           }}
           className={`text-center capitalize pb-2 text-lg ${
             i === currentIndex
-              ? " font-[600] text-primary-tint-3"
-              : " text-neutral-7"
+              ? " font-[600] text-primary-tint-3 dark:text-white"
+              : " text-neutral-7 dark:text-neutral-6"
           }`}
         >
           {item}
         </button>
       ))}
       <div
-        className="h-[2px] bg-primary-tint-3 absolute bottom-[-2px] transition-all duration-500"
+        className="h-[2px] bg-primary-tint-3 dark:bg-white absolute bottom-[-2px] transition-all duration-500"
         style={{
           width: `calc(100% / ${items.length})`,
           marginLeft: `calc((100% / ${items.length}) * ${currentIndex})`,
