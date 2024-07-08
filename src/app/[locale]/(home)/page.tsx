f4bdbb032fccd20d08442d9b7ba3d7a3bafc4c34
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-08 11:36:58 +0300

    feat: pinned dashboards

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -1,5 +1,6 @@
 import HomePinnedAppletes from "@/components/HomePinnedApplets";
-import { IApplet } from "@/types/general";
+import HomePinnedDashboardes from "@/components/HomePinnedDashboards";
+import { IApplet, IDashboard } from "@/types/general";
 
 export default function Home() {
   const pinnedApplets: IApplet[] = [
@@ -28,9 +29,36 @@ export default function Home() {
       image: "/assets/thing.jpeg",
     },
   ];
+  const pinnedDashboards: IDashboard[] = [
+    {
+      name: "Dashboard's name",
+      description: "Dashboard's description",
+      pin: true,
+      image: "/assets/thing.jpeg",
+    },
+    {
+      name: "Dashboard's name",
+      description: "Dashboard's description",
+      pin: true,
+      image: "/assets/thing.jpeg",
+    },
+    {
+      name: "Dashboard's name",
+      description: "Dashboard's description",
+      pin: true,
+      image: "/assets/thing.jpeg",
+    },
+    {
+      name: "Dashboard's name",
+      description: "Dashboard's description",
+      pin: true,
+      image: "/assets/thing.jpeg",
+    },
+  ];
   return (
-    <main className="flex flex-col py-6 px-4">
+    <main className="flex flex-col py-6 px-4 md:flex-row md:gap-4 lg:gap-10">
       <HomePinnedAppletes appletes={pinnedApplets} />
+      <HomePinnedDashboardes dashboards={pinnedDashboards} />
     </main>
   );
 }
