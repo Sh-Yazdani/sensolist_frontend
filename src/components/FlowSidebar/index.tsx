25b7b54f21877bd99b21f083e61965a9b0f8e74e
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-14 11:03:56 +0300

    feat: mobile view

--- src/components/FlowSidebar/index.tsx
+++ src/components/FlowSidebar/index.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Accordion } from "flowbite-react";
+import { ArrowLeft2, ArrowRight2 } from "iconsax-react";
+import { useState } from "react";
 import {
   actionNodes,
   controlNodes,
@@ -8,6 +10,7 @@ import {
   triggerNodes,
 } from "./nodeItems";
 export default function FlowSidebar() {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
   const onDragStart = (
     event: {
       dataTransfer: {
@@ -23,89 +26,106 @@ export default function FlowSidebar() {
     event.dataTransfer.effectAllowed = "move";
   };
   return (
-    <div className="flex flex-col w-[280px] h-auto">
-      <Accordion>
-        <Accordion.Panel>
-          <Accordion.Title>Triggers</Accordion.Title>
-          <Accordion.Content>
-            {triggerNodes.map((item, i) => (
-              <div
-                key={item.value}
-                className={`${
-                  i !== 0 && "mt-4"
-                } border border-neutral-6 px-4 py-2 rounded-lg flex items-center dark:text-neutral-4`}
-                onDragStart={(event) =>
-                  onDragStart(event, "triggerNode", item.value)
-                }
-                draggable
-              >
-                {item.icon}
-                <span className="ml-2">{item.name}</span>
-              </div>
-            ))}
-          </Accordion.Content>
-        </Accordion.Panel>
-        <Accordion.Panel>
-          <Accordion.Title>Actions</Accordion.Title>
-          <Accordion.Content>
-            {actionNodes.map((item, i) => (
-              <div
-                key={item.value}
-                className={`${
-                  i !== 0 && "mt-4"
-                } border border-neutral-6 px-4 py-2 rounded-lg flex items-center dark:text-neutral-4`}
-                onDragStart={(event) =>
-                  onDragStart(event, "triggerNode", item.value)
-                }
-                draggable
-              >
-                {item.icon}
-                <span className="ml-2">{item.name}</span>
-              </div>
-            ))}
-          </Accordion.Content>
-        </Accordion.Panel>
-        <Accordion.Panel>
-          <Accordion.Title>Flow Controls</Accordion.Title>
-          <Accordion.Content>
-            {controlNodes.map((item, i) => (
-              <div
-                key={item.value}
-                className={`${
-                  i !== 0 && "mt-4"
-                } border border-neutral-6 px-4 py-2 rounded-lg flex items-center dark:text-neutral-4`}
-                onDragStart={(event) =>
-                  onDragStart(event, "triggerNode", item.value)
-                }
-                draggable
-              >
-                {item.icon}
-                <span className="ml-2">{item.name}</span>
-              </div>
-            ))}
-          </Accordion.Content>
-        </Accordion.Panel>
-        <Accordion.Panel>
-          <Accordion.Title>Things</Accordion.Title>
-          <Accordion.Content>
-            {thingNodes.map((item, i) => (
-              <div
-                key={item.value}
-                className={`${
-                  i !== 0 && "mt-4"
-                } border border-neutral-6 px-4 py-2 rounded-lg flex items-center dark:text-neutral-4`}
-                onDragStart={(event) =>
-                  onDragStart(event, "triggerNode", item.value)
-                }
-                draggable
-              >
-                {item.icon}
-                <span className="ml-2">{item.name}</span>
-              </div>
-            ))}
-          </Accordion.Content>
-        </Accordion.Panel>
-      </Accordion>
+    <div
+      className={` relative transition-all ${
+        isOpen ? "visiblel" : " invisible w-0"
+      }`}
+    >
+      <button
+        onClick={() => {
+          setIsOpen((prev) => !prev);
+        }}
+        className="absolute md:hidden bg-neutral-2 py-3 right-[-1.5rem] top-10 rounded-r-xl z-20 visible"
+      >
+        {isOpen ? <ArrowLeft2 /> : <ArrowRight2 />}
+      </button>
+      <div
+        className={`flex flex-col w-[280px] h-autoz-20 bg-white dark:bg-primary-Shade-1
+        `}
+      >
+        <Accordion>
+          <Accordion.Panel>
+            <Accordion.Title>Triggers</Accordion.Title>
+            <Accordion.Content>
+              {triggerNodes.map((item, i) => (
+                <div
+                  key={item.value}
+                  className={`${
+                    i !== 0 && "mt-4"
+                  } border border-neutral-6 px-4 py-2 rounded-lg flex items-center dark:text-neutral-4`}
+                  onDragStart={(event) =>
+                    onDragStart(event, "triggerNode", item.value)
+                  }
+                  draggable
+                >
+                  {item.icon}
+                  <span className="ml-2">{item.name}</span>
+                </div>
+              ))}
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>Actions</Accordion.Title>
+            <Accordion.Content>
+              {actionNodes.map((item, i) => (
+                <div
+                  key={item.value}
+                  className={`${
+                    i !== 0 && "mt-4"
+                  } border border-neutral-6 px-4 py-2 rounded-lg flex items-center dark:text-neutral-4`}
+                  onDragStart={(event) =>
+                    onDragStart(event, "triggerNode", item.value)
+                  }
+                  draggable
+                >
+                  {item.icon}
+                  <span className="ml-2">{item.name}</span>
+                </div>
+              ))}
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>Flow Controls</Accordion.Title>
+            <Accordion.Content>
+              {controlNodes.map((item, i) => (
+                <div
+                  key={item.value}
+                  className={`${
+                    i !== 0 && "mt-4"
+                  } border border-neutral-6 px-4 py-2 rounded-lg flex items-center dark:text-neutral-4`}
+                  onDragStart={(event) =>
+                    onDragStart(event, "triggerNode", item.value)
+                  }
+                  draggable
+                >
+                  {item.icon}
+                  <span className="ml-2">{item.name}</span>
+                </div>
+              ))}
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>Things</Accordion.Title>
+            <Accordion.Content>
+              {thingNodes.map((item, i) => (
+                <div
+                  key={item.value}
+                  className={`${
+                    i !== 0 && "mt-4"
+                  } border border-neutral-6 px-4 py-2 rounded-lg flex items-center dark:text-neutral-4`}
+                  onDragStart={(event) =>
+                    onDragStart(event, "triggerNode", item.value)
+                  }
+                  draggable
+                >
+                  {item.icon}
+                  <span className="ml-2">{item.name}</span>
+                </div>
+              ))}
+            </Accordion.Content>
+          </Accordion.Panel>
+        </Accordion>
+      </div>
     </div>
   );
 }
