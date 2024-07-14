afb55fe09267fc9b8da6d4aa3b0eecde6981cd2f
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-14 17:46:21 +0300

    feat: implement widgets

--- src/components/DashboardWidgetSelect/index.tsx
+++ src/components/DashboardWidgetSelect/index.tsx
@@ -1,9 +1,37 @@
 import { Close } from "@mui/icons-material";
+import Image from "next/image";
 
 interface DashboardWidgetSelectProps {
   isOpen: boolean;
   onClose: () => void;
 }
+
+const widgets = [
+  {
+    name: "chart",
+    image: "/assets/widgets/chart.svg",
+  },
+  {
+    name: "buttons",
+    image: "/assets/widgets/buttons.svg",
+  },
+  {
+    name: "count",
+    image: "/assets/widgets/count.png",
+  },
+  {
+    name: "guage",
+    image: "/assets/widgets/guage.png",
+  },
+  {
+    name: "map",
+    image: "/assets/widgets/map.png",
+  },
+  {
+    name: "table",
+    image: "/assets/widgets/table.png",
+  },
+];
 
 export default function DashboardWidgetSelect({
   isOpen,
@@ -20,6 +48,19 @@ export default function DashboardWidgetSelect({
         <button onClick={onClose} className="ml-auto">
           <Close className=" text-white" />
         </button>
+      </div>
+      <div className=" flex flex-wrap gap-2 pl-2 pt-4">
+        {widgets.map((wdg) => (
+          <div
+            key={wdg.name}
+            className=" bg-white shadow-lg flex flex-col w-[calc(25%-8px)] p-4 hover:shadow-neutral-6 cursor-pointer"
+          >
+            <div className=" capitalize text-sm mb-2">{wdg.name}</div>
+            <div className="relative w-full aspect-square">
+              <Image fill src={wdg.image} alt="widget name" />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
