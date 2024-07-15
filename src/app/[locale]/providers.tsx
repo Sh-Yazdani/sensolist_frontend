2cf47fef476d5f79b58ee17daea6d9e44b66592d
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-15 18:25:22 +0300

    feat: set default theme to dark

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -13,7 +13,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   }
   return (
     <Provider store={storeRef.current}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         {children}
       </ThemeProvider>
     </Provider>
