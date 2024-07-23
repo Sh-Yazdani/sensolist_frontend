06e9de19bde6e4bad794f9feccfe4ad81c08d58b
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-07-23 17:49:27 +0300

    feat: add refrences node modal

---
+++ src/components/FlowContent/RefrencesFormModal.tsx
@@ -0,0 +1,83 @@
+import { NodeDataType } from "@/types/general";
+import { Node } from "@xyflow/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import Button from "../UI/Button";
+import Input from "../UI/Input";
+import Modal from "../UI/Modal";
+
+interface RefrencesFormModalProps {
+  open: boolean;
+  onClose: () => void;
+  node: Node<NodeDataType> | null;
+  onAddNode: () => void;
+}
+
+interface ICreateNodeInputs {
+  title: string;
+  description?: string;
+}
+
+export default function RefrencesFormModal({
+  open,
+  onClose,
+  node,
+  onAddNode,
+}: RefrencesFormModalProps) {
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm<ICreateNodeInputs>();
+
+  const onSubmit: SubmitHandler<ICreateNodeInputs> = (data) => {
+    console.log("submit", data);
+    onAddNode();
+    reset();
+    onClose();
+  };
+  return (
+    <Modal onClose={onClose} open={open}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          required
+          error={
+            errors.title?.type === "required" ? "This field is required" : ""
+          }
+          label="Title"
+          register={register}
+          name="title"
+          className="mt-6"
+        />
+        <Input
+          error={
+            errors.description?.type === "required"
+              ? "This field is required"
+              : ""
+          }
+          label="Description"
+          register={register}
+          name="description"
+          className="mt-6"
+        />
+        <div className="flex items-center gap-4 mt-8">
+          <Button
+            onClick={(event: React.MouseEvent<HTMLElement>) => {
+              event.preventDefault();
+              reset();
+              onClose();
+            }}
+            className="w-[36%]"
+            variant="secondary"
+          >
+            Cancel
+          </Button>
+          <Button className="w-[64%]" type="submit">
+            Create
+          </Button>
+        </div>
+      </form>
+    </Modal>
+  );
+}
