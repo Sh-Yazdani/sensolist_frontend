74fcd54281ae6dc53b71ab0be2b10f94a1782075
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-08-05 21:47:47 +0300

    feat: add secret to next-auth

--- src/app/[locale]/(home)/dashboard/[slug]/page.tsx
+++ src/app/[locale]/(home)/dashboard/[slug]/page.tsx
@@ -3,7 +3,7 @@ import DashboardWidgets from "@/components/Widgets";
 // async function getData() {
 //   const headers = {
 //     Authorization:
-//       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsidXNlcklkIjoiNjZhZjVmZjdmODk1Yzk3NTc5YzdlZTcyIiwicGhvbmVudW1iZXIiOiIrMTkxMzk2MzM1ODkiLCJzeXN0ZW1Sb2xlIjoiQWRtaW4ifSwiaWF0IjoxNzIyNzg3MDEzLCJleHAiOjE3MjI3ODc5MTN9.RLKAV6D7Nw51z139mg9tOagHOwGsUjyPbga8JCvgtqA",
+//       "Bearer ",
 //   };
 //   const res = await fetch(
 //     "http://185.110.189.232:3123/api/data?sender=sns0000001",
